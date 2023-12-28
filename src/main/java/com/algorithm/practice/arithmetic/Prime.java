240b86fef01ca42eeced776d0a8e6334682c11d8
Rabindra Rakshit <rovir2r@gmail.com>
2023-12-28 08:07:20 +0100

    Prime Complexity

--- src/main/java/com/algorithm/practice/arithmetic/Prime.java
+++ src/main/java/com/algorithm/practice/arithmetic/Prime.java
@@ -1,6 +1,10 @@
 package com.algorithm.practice.arithmetic;
 
 public class Prime {
+
+    // Time Complexity: O(sqrt(n))
+    // Space Complexity : O(1)
+
     static boolean checkForPrime(int n){
         for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
