7a7bffe71c19b0452eafe5903b85a666a6af929a
Rabindra Rakshit <rovir2r@gmail.com>
2024-01-19 22:05:04 +0100

    Longest Palindromic SubString centre expand

--- src/main/java/com/algorithm/practice/string/medium/LongestPalindromicSubString.java
+++ src/main/java/com/algorithm/practice/string/medium/LongestPalindromicSubString.java
@@ -8,7 +8,7 @@ public class LongestPalindromicSubString {
     // Time Complexity: O(n^3)
     // Space Complexity : O(1)
     private static String longestNaive(String str) {
-        String result = null;
+        String result = String.valueOf(str.charAt(0));
         int maxLength = 0;
         for (int i = 0; i < str.length(); i++) {
             for (int j = i + 1; j < str.length(); j++) {
@@ -20,6 +20,37 @@ public class LongestPalindromicSubString {
             }
         }
         return result;
+    }
+
+    // Time Complexity: O(n^2)
+    // Space Complexit: O(1)
+    private static String longestExpandFromCenter(String str) {
+        String maxStr = str.substring(0, 1);
+        for (int i = 0; i < str.length(); i++) {
+            // for odd
+            String oddStr = getPalindromeSubStr(str, i, i);
+            if (oddStr.length() > maxStr.length()) {
+                maxStr = oddStr;
+            }
+
+            // for even
+            String evenStr = getPalindromeSubStr(str, i, i + 1);
+            if (evenStr.length() > maxStr.length()) {
+                maxStr = evenStr;
+            }
+        }
+        return maxStr;
+    }
+
+    private static String getPalindromeSubStr(String str, int left, int right) {
+        while (left >= 0 && right < str.length()) {
+            if (str.charAt(left) != str.charAt(right)) {
+                break;
+            }
+            left--;
+            right++;
+        }
+        return str.substring(left + 1, right);
     }
 
     private static boolean isPalindrome(String str) {
@@ -39,5 +70,8 @@ public class LongestPalindromicSubString {
     public static void main(String[] args) {
         System.out.println(longestNaive("ababc"));
         System.out.println(longestNaive("abc"));
+
+        System.out.println(longestExpandFromCenter("ababc"));
+        System.out.println(longestExpandFromCenter("abc"));
     }
 }
