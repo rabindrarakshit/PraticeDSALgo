ab8a6fa2253aa69d80f519bd6fde4eeb971f7fae
Rabindra Rakshit <rovir2r@gmail.com>
2024-02-02 19:35:31 +0100

    generate all binary strings

---
+++ src/main/java/com/algorithm/practice/recursion/GenerateAllBinaryStrings.java
@@ -0,0 +1,20 @@
+package com.algorithm.practice.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateAllBinaryStrings {
+    static List<String> generateAllBinaryStrings(int n, String s, List<String> list) {
+        if (n <= 0) {
+            list.add(s);
+            return list;
+        }
+        generateAllBinaryStrings(n - 1, s + "0", list);
+        generateAllBinaryStrings(n - 1, s + "1", list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        generateAllBinaryStrings(3,new String(), new ArrayList<>()).forEach(a -> System.out.print(a + " "));
+    }
+}
