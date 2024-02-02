f7652979eb40a1028800c8bee65b12f48beb8df9
Rabindra Rakshit <rovir2r@gmail.com>
2024-02-02 20:23:11 +0100

    generate all valid parenthesis pairs

---
+++ src/main/java/com/algorithm/practice/recursion/GenerateValidParenthesisPairCombinations.java
@@ -0,0 +1,24 @@
+package com.algorithm.practice.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateValidParenthesisPairCombinations {
+    static List<String> generate(String str, int max, List<String> list, int open, int close) {
+        if (str.length() == max * 2) {
+            list.add(str);
+            return list;
+        }
+        if (open < max) {
+            generate(str + "(", max, list, open + 1, close);
+        }
+        if (close < open) {
+            generate(str + ")", max, list, open, close + 1);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        generate("", 3, new ArrayList<>(), 0, 0).forEach(ch -> System.out.println(ch));
+    }
+}
