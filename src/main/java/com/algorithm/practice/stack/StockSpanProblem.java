5201134fe8919bf4e4ab829597f968a3b3a80a00
Rabindra Rakshit <rovir2r@gmail.com>
2024-02-14 21:07:46 +0100

    Stock Span problem

---
+++ src/main/java/com/algorithm/practice/stack/StockSpanProblem.java
@@ -0,0 +1,45 @@
+package com.algorithm.practice.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class StockSpanProblem {
+    List<Integer> stockValues = new ArrayList<>();
+    Stack<int[]> stack = new Stack<>();
+
+    public StockSpanProblem() {
+    }
+
+    public int next1(int price) {
+        int count = 1;
+        stockValues.add(price);
+        for (int i = stockValues.size() - 2; i >= 0; i--) {
+            if (stockValues.get(i) <= price) {
+                count += 1;
+            } else break;
+        }
+        return count;
+    }
+
+    public int next(int price) {
+        int res = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price)
+            res += stack.pop()[1];
+        stack.push(new int[]{price, res});
+        return res;
+    }
+}
+
+class TestStock {
+    public static void main(String[] args) {
+        StockSpanProblem stockSpanner = new StockSpanProblem();
+        System.out.println(stockSpanner.next(100));
+        System.out.println(stockSpanner.next(80));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(70));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(75));
+        System.out.println(stockSpanner.next(85));
+    }
+}
