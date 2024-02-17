5455164caa1d378118ea86d79bb3d18662c480ac
Rabindra Rakshit <rovir2r@gmail.com>
2024-02-17 17:52:24 +0100

    FruitIntoBaskets

---
+++ src/main/java/com/algorithm/practice/slidingwindowandtwopointer/FruitIntoBaskets.java
@@ -0,0 +1,8 @@
+package com.algorithm.practice.slidingwindowandtwopointer;
+
+// Ref: https://leetcode.com/problems/fruit-into-baskets/description/
+public class FruitIntoBaskets {
+    static int maxFruits(int[] arr, int k){
+        return 0;
+    }
+}
