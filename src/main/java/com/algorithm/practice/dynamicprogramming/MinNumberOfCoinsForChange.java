dfd0f2fd4c067c5dc9ac0c016ca35c1955e8f570
Rabindra Rakshit <rovir2r@gmail.com>
2024-03-17 11:31:16 +0100

    DP Algos

---
+++ src/main/java/com/algorithm/practice/dynamicprogramming/MinNumberOfCoinsForChange.java
@@ -0,0 +1,31 @@
+package com.algorithm.practice.dynamicprogramming;
+
+import java.util.Arrays;
+
+//Ref: https://www.algoexpert.io/questions/min-number-of-coins-for-change
+//Time: O(nd) | Space: O(n)
+
+public class MinNumberOfCoinsForChange {
+    public static int minNumberOfCoinsForChange(int n, int[] denoms) {
+        int[] numOfCoins = new int[n+1];
+        Arrays.fill(numOfCoins, Integer.MAX_VALUE);
+        numOfCoins[0]=0;
+        int toCompare = 0;
+        for(int denom: denoms){
+            for(int amount=1;amount<numOfCoins.length;amount++){
+                if(denom<=amount){
+                    if(numOfCoins[amount-denom]==Integer.MAX_VALUE){
+                        toCompare = numOfCoins[amount-denom];
+                    }else{
+                        toCompare = numOfCoins[amount-denom]+1;
+                    }
+                    numOfCoins[amount] = Math.min(numOfCoins[amount], toCompare);
+                }
+            }
+        }
+        return numOfCoins[n] == Integer.MAX_VALUE? -1: numOfCoins[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(minNumberOfCoinsForChange(9, new int[]{3,5}));
+    }
+}
---
+++ src/main/java/com/algorithm/practice/dynamicprogramming/NumberOfWaysToMakeChange.java
@@ -0,0 +1,19 @@
+package com.algorithm.practice.dynamicprogramming;
+
+// Ref: https://www.algoexpert.io/questions/number-of-ways-to-make-change
+// Time: O(mn) | Space: O(n)
+
+public class NumberOfWaysToMakeChange {
+    public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+        int[] ways = new int[n+1];
+        ways[0]=1;
+        for(int i=0;i<denoms.length;i++){
+            for(int j=1;j<=n;j++){
+                if(j-denoms[i]>=0){
+                    ways[j]+= ways[j-denoms[i]];
+                }
+            }
+        }
+        return ways[n];
+    }
+}
