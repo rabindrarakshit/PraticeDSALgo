c664c67f0c9cd8220deea5e467c4ac937a8000e3
Rabindra Rakshit <rovir2r@gmail.com>
2024-03-29 11:53:14 +0100

    Number oF Longest Sub Sequence

--- src/main/java/com/algorithm/practice/dynamicprogramming/lis/NumberOfLongestSubSequences.java
+++ src/main/java/com/algorithm/practice/dynamicprogramming/lis/NumberOfLongestSubSequences.java
@@ -1,2 +1,37 @@
-package com.algorithm.practice.dynamicprogramming.lis;public class NumberOfLongestSubSequences {
+package com.algorithm.practice.dynamicprogramming.lis;
+
+import java.util.Arrays;
+
+// Ref: https://takeuforward.org/data-structure/number-of-longest-increasing-subsequences-dp-47/
+
+public class NumberOfLongestSubSequences {
+    static int numberOfLongestSubSequence(int[] arr) {
+        int[] dp = new int[arr.length];
+        int[] ct = new int[arr.length];
+
+        Arrays.fill(dp, 1);
+        Arrays.fill(ct, 1);
+        int max =1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    if (dp[j] + 1 == dp[i]) {
+                        ct[i] = ct[i] + ct[j];
+                    }
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+            }
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        int numberOfLongestSub = 0;
+        for(int i=0; i< arr.length; i++){
+            if(dp[i]==max) numberOfLongestSub+=ct[i];
+        }
+
+        return numberOfLongestSub;
+    }
 }
