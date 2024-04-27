55a456fdd56c596c17ab35e7d6c460655a2e9773
Rabindra Rakshit <rovir2r@gmail.com>
2024-04-27 22:06:00 +0200

    Graph - Floyd Warshall

--- src/main/java/com/algorithm/practice/graph/striver/shortestpath/FloydWarshall.java
+++ src/main/java/com/algorithm/practice/graph/striver/shortestpath/FloydWarshall.java
@@ -1,2 +1,54 @@
-package com.algorithm.practice.graph.striver.shortestpath;public class FloydWarshall {
+package com.algorithm.practice.graph.striver.shortestpath;
+
+// Ref: https://takeuforward.org/data-structure/floyd-warshall-algorithm-g-42/
+
+/*
+* Time Complexity: O(V3), as we have three nested loops each running for V times, where V = no. of vertices.
+
+Space Complexity: O(V2), where V = no. of vertices. This space complexity is due to storing the adjacency matrix of the given graph.
+*
+* If we apply Dijkstra here (incase of no negative loops) then we have a complexity of O(VElog(V))
+* */
+
+public class FloydWarshall {
+    static int[][] shortestPath(int[][] graph) {
+        int nodes = graph.length;
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                if (graph[i][j] == -1) {
+                    graph[i][j] = (int) 1e9;
+                }
+                if (i == j) {
+                    graph[i][j] = 0;
+                }
+            }
+        }
+
+        for (int k = 0; k < nodes; k++) {
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        // incase its mentioned graph has negative edges
+        /*for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                if (graph[i][j] < 0) {
+                    return new int[][]{{-1}};
+                }
+            }
+        }*/
+
+
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                if (graph[i][j] == (int) 1e9) {
+                    graph[i][j] = -1;
+                }
+            }
+        }
+        return graph;
+    }
 }
