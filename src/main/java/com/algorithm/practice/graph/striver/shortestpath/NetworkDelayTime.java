042b8eb021895cc689c8695a5fafabec29279449
Rabindra Rakshit <rovir2r@gmail.com>
2024-05-10 09:40:27 +0200

    Graph - Ways To Arrive

--- src/main/java/com/algorithm/practice/graph/striver/shortestpath/NetworkDelayTime.java
+++ src/main/java/com/algorithm/practice/graph/striver/shortestpath/NetworkDelayTime.java
@@ -1,2 +1,81 @@
-package com.algorithm.practice.graph.striver.shortestpath;public class NetworkDelayTime {
+package com.algorithm.practice.graph.striver.shortestpath;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// Ref: https://leetcode.com/problems/network-delay-time/description/
+// time: O(v+e)
+// space: O(v+e)
+public class NetworkDelayTime {
+
+    static class Node {
+        int nodeId;
+        int delay;
+
+        Node(int nodeId, int delay) {
+            this.nodeId = nodeId;
+            this.delay = delay;
+        }
+    }
+
+    static List<List<Node>> createAdjList(int[][] networkArray, int nodes) {
+        List<List<Node>> networkList = new ArrayList<>();
+        for (int i = 0; i < nodes; i++) {
+            networkList.add(new ArrayList<>());
+        }
+        for (int[] network : networkArray) {
+            int source = network[0];
+            int target = network[1];
+            int delay = network[2];
+
+            networkList.get(source).add(new Node(target, delay));
+        }
+        return networkList;
+    }
+
+    static int minimumDelay(int[][] networkArray, int nodes, int src) {
+        nodes++; // Only because leetcode had it 1 based
+        List<List<Node>> networkList = createAdjList(networkArray, nodes);
+        int[] delays = new int[nodes];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < nodes; i++) {
+            delays[i] = (int) 1e9;
+        }
+
+        q.add(src);
+        delays[src] = 0;
+        delays[0] = 0; // Because its One based
+        while (!q.isEmpty()) {
+            int nodeId = q.poll();
+            for (Node adjNode : networkList.get(nodeId)) {
+                int currentNodeId = adjNode.nodeId;
+                int currentNodeDelay = adjNode.delay + delays[nodeId];
+
+                if (currentNodeDelay < delays[currentNodeId]) {
+                    delays[currentNodeId] = currentNodeDelay;
+                    q.add(currentNodeId);
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < delays.length; i++) {
+            if (delays[i] == (int) 1e9) {
+                return -1;
+            } else if (delays[i] > max) {
+                max = delays[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] networkArray = {{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
+        int ans = minimumDelay(networkArray, 4, 2);
+
+        System.out.print(ans);
+        System.out.println();
+    }
 }
--- src/main/java/com/algorithm/practice/graph/striver/shortestpath/WaysToArrive.java
+++ src/main/java/com/algorithm/practice/graph/striver/shortestpath/WaysToArrive.java
@@ -1,2 +1,79 @@
-package com.algorithm.practice.graph.striver.shortestpath;public class WaysToArrive {
+package com.algorithm.practice.graph.striver.shortestpath;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+// Ref: https://takeuforward.org/data-structure/g-40-number-of-ways-to-arrive-at-destination/
+
+public class WaysToArrive {
+
+    static class Node {
+        int nodeId;
+        int weight;
+
+        Node(int nodeId, int weight) {
+            this.nodeId = nodeId;
+            this.weight = weight;
+        }
+    }
+
+    static List<List<Node>> createAdjList(int[][] roadsArray, int nodes) {
+        List<List<Node>> roadsList = new ArrayList<>();
+        for (int i = 0; i < nodes; i++) {
+            roadsList.add(new ArrayList<>());
+        }
+        for (int[] network : roadsArray) {
+            int source = network[0];
+            int target = network[1];
+            int delay = network[2];
+
+            roadsList.get(source).add(new Node(target, delay));
+            roadsList.get(target).add(new Node(source, delay));
+        }
+        return roadsList;
+    }
+
+    static int waysToArrive(int[][] roads, int src, int dest, int nodes) {
+        List<List<Node>> roadsList = createAdjList(roads, nodes);
+        int mod = (int) (1e9 + 7);
+        int[][] weightFrequency = new int[nodes][2]; // 0-> weight 1->freq
+
+        for (int i = 0; i < nodes; i++) {
+            weightFrequency[i][0] = (int) 1e9;
+        }
+
+        weightFrequency[src][0] = 0;
+        weightFrequency[src][1] = 1;
+
+        PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.comparingInt(a -> a));
+        q.add(src);
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            for (Node adjNode : roadsList.get(node)) {
+                int destination = adjNode.nodeId;
+                int weight = adjNode.weight + weightFrequency[node][0];
+
+                if (weight < weightFrequency[destination][0]) {
+                    weightFrequency[destination][0] = weight;
+                    weightFrequency[destination][1] = weightFrequency[node][1];
+                    q.add(adjNode.nodeId);
+                } else if (weight == weightFrequency[destination][0]) {
+                    weightFrequency[destination][1] = (weightFrequency[node][1] + weightFrequency[destination][1]) % mod;
+                }
+            }
+        }
+        return weightFrequency[dest][1] % mod;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        int[][] roadsArray = {{0, 6, 7}, {0, 1, 2}, {1, 2, 3}, {1, 3, 3}, {6, 3, 3}, {3, 5, 1}, {6, 5, 1}, {2, 5, 1}, {0, 4, 5}, {4, 6, 2}};
+        int ans = waysToArrive(roadsArray, 0, n - 1, n);
+
+        System.out.print(ans);
+        System.out.println();
+    }
 }
